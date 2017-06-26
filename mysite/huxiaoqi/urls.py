f103f1ef0882326a4d7fa9d9bf6cce569b2ccc1b
huyuqi <yuqi.hu@gmail.com>
2017-06-26 10:19:12 +0800

    edit urls.py

--- mysite/huxiaoqi/urls.py
+++ mysite/huxiaoqi/urls.py
@@ -1,5 +1,6 @@
+# mysite/huxiaoqi/urls.py
 from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'^$', views.index,name='index'), ]
+urlpatterns = [url(r'^$', views.index,name='index'),]
