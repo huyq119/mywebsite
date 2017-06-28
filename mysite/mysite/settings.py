096347fb06ce8e0db9ea7434370002134e17898a
huyuqi <yuqi.hu@gmail.com>
2017-06-28 17:53:48 +0800

    update code

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'huxiaoqi.apps.HuxiaoqiConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
