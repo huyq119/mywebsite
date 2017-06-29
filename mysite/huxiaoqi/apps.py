70ba7421920b2312a1a904141c715ba6ce2ec1e4
huyuqi <yuqi.hu@gmail.com>
2017-06-29 17:23:00 +0800

    delete all files

--- mysite/huxiaoqi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HuxiaoqiConfig(AppConfig):
-    name = 'huxiaoqi'
--- mysite/huxiaoqi/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("你好,世界,我在这里等你很久了")
-
